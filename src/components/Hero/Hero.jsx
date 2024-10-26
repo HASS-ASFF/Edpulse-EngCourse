a28e146de5d2c4bd82bd578b4cd3af5ccacd8704
HASS-ASFF <hassanasffar22@gmail.com>
2024-10-26 15:31:10 +0100

    final updates 1

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { motion } from 'framer-motion';
 import './Hero.css'
 import dark_arrow from '../../assets/black-Arrow.png'
+import cover from '../../assets/cover.webp'
 
 const Hero = () => {
   return (
     <div className='hero container'>
+      <img src={cover} alt="" className='hero-img'/>
         <motion.div
         initial={{ opacity: 0, scale: 0.5 }}
         animate={{ opacity: 1, scale: 1 }}
